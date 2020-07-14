2e33cd640990c9543f6e07bcf75131bc59480098
Abhijeet Shikhar <shikharabhijeet1995@gmail.com>
2020-07-14 17:04:10 +0530

    -------

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,9 @@
 import {applyMiddleware, createStore} from "redux";
 import {rootReducer} from "./rootReducer";
 import {composeWithDevTools} from "redux-devtools-extension";
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
-let middleWares = [logger , thunk];
+let middleWares = [thunk];
 
 let store = createStore(rootReducer,composeWithDevTools(applyMiddleware(...middleWares)));
 export {store};
